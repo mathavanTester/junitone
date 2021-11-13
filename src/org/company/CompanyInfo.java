ec7116f91245c9d34f7985ef5c1b89fc32906e79
mathavaTester <maddy70149@gmail.com>
2021-11-13 23:03:10 +0530

    new project

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,21 @@
+package org.company;
+
+
+
+public class CompanyInfo {
+	public void CompanyName() {
+	System.out.println("vpr.pvt");
+}
+	public void Companyid() {
+		System.out.println("20123");
+	}
+	public void CompanyAdd() {
+		System.out.println("t.nager");
+	}
+	public static void main(String[] args) {
+		CompanyInfo a = new CompanyInfo();
+		a.CompanyName();
+		a.Companyid();
+		a.CompanyAdd();
+	}
+}
